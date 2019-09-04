5f6685fdd1f18757d5c246964e2a8529dd198395
FireNinja <JimmyMcBride>
2019-09-04 18:01:53 -0500

    updated axios end points to deployed urls

--- src/actions/logoutAction.js
+++ src/actions/logoutAction.js
@@ -7,7 +7,7 @@ export const LOGOUT_FAILURE = 'DELETE_DATA_FAILURE'
 export const logout = user => dispatch => {
   dispatch({ type: LOGOUT_START })
   axios
-      .delete('localhost:4000/api/auth/logout')
+      .delete('https://dadjokes-backend.herokuapp.com//api/auth/logout')
       .then(res => {
           dispatch({ 
             type: LOGOUT_SUCCESS, 
