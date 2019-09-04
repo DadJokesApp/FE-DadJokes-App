7ace0fa7f368fcb6c7f17e2979bccc6ad8a36ccd
FireNinja <JimmyMcBride>
2019-09-04 18:43:10 -0500

    user

--- src/components/Auth/PrivateRoute.js
+++ src/components/Auth/PrivateRoute.js
@@ -2,19 +2,19 @@ import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 
-const PrivateRoute = ({ component: Component, isLoggedIn, ...rest }) => {
-  console.log(isLoggedIn)
+const PrivateRoute = ({ component: Component, user, ...rest }) => {
+  console.log(user)
   return (
     <Route
       {...rest}
-      render={props => isLoggedIn ? <Component {...props} /> : <Redirect to="/login" />}
+      render={props => user ? <Component {...props} /> : <Redirect to="/login" />}
     />
   )
 }
 
 const mapStateToProps = state => {
   return {
-    isLoggedIn: state.isLoggedIn,
+    user: state.user,
   }
 }
 
