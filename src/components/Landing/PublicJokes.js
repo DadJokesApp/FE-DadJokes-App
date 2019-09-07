908c710b5bfeade32d34616503830f4e4a2133eb
Mayankho <yankhot96@gmail.com>
2019-09-06 23:50:04 -0400

    Jokes component set no styling

--- src/components/Landing/PublicJokes.js
+++ src/components/Landing/PublicJokes.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import axios from 'axios';
+
 
 export default class PublicJokes extends React.Component {
+  state = {jokes : [],loading : false};
+  static defaultProps = {
+    numJokesToGet: 4
+  }
+  componentDidMount(){
+  
+  }
+  getJokes = async () => {
+    let publicJokes = [];
+    let pull = axios.get('https://dadjokes-backend.herokuapp.com/api/jokes/public');
+
+  }
+
   render() {
+    if (this.state.loading) {
+      return(
+        <div>
+          <h1>Jokes Loading...</h1>
+        </div>
+      );
+    }
+    
     return (
-      <div>Public Jokes go here</div>
+      <div>
+        {pull}
+      </div>
     )
   }
 }
