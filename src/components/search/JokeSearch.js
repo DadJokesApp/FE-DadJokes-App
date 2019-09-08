ae07e842f4dec21c9b22ccb219a367a72897a491
Mayankho <yankhot96@gmail.com>
2019-09-07 22:35:34 -0400

    Search form and friends

---
+++ src/components/search/JokeSearch.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+
+
+class SearchForm extends Component {
+    state = {jokes: '', searchText: ''}
+ 
+    onSearchSubmit = (event) => {
+        event.preventDefault();
+        axios.get('https://dadjokes-backend.herokuapp.com/api/jokes/public')
+        .then (response => {
+            this.setState({jokes: response.data.filter(joke => {
+                return joke.joke.toLowerCase().includes(this.state.searchText.toLowerCase())
+            })})
+            this.props.searchJoke(this.state.jokes)
+        })
+    }
+
+    render(){
+        return(
+            <div className= "ui segment">
+                <form onSubmit={this.onSearchSubmit} className="ui form">
+                    <div className ="field">
+                        <h1><label htmlFor="search">Search Jokes</label></h1>
+                    </div>
+                    <input type = "text" value={this.state.searchText} onChange={(e) => this.setState({searchText: e.target.value})} ></input>
+                </form>
+
+            </div>
+        )
+    }
+}
+
+
+export default SearchForm
